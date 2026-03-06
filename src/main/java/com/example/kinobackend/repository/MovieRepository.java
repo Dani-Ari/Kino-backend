f1fefa367f6a9d94b90a66f0e074878ff933cf51
git-arian <195733915+git-arian@users.noreply.github.com>
2026-03-06 11:19:22 +0100

    feat(SCRUM-19): Create Movie repository

---
+++ src/main/java/com/example/kinobackend/repository/MovieRepository.java
@@ -0,0 +1,8 @@
+package com.example.kinobackend.repository;
+
+import com.example.kinobackend.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+}
