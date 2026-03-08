bc054434e242b22da9f9ce58d2383fb5841ce9a5
git-arian <195733915+git-arian@users.noreply.github.com>
2026-03-08 13:15:09 +0100

    feat(SCRUM-24,SCRUM-25,SCRUM-27): implement create showing endpoint, link movie to showing and theatre + time

---
+++ src/main/java/com/example/kinobackend/service/ShowingService.java
@@ -0,0 +1,28 @@
+package com.example.kinobackend.service;
+
+import com.example.kinobackend.model.Showing;
+import com.example.kinobackend.repository.ShowingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ShowingService {
+    private final ShowingRepository showingRepository;
+
+    public ShowingService(ShowingRepository showingRepository) {
+        this.showingRepository = showingRepository;
+    }
+
+    public List<Showing> getAllShowings() {
+        return showingRepository.findAll();
+    }
+
+    public Showing addShowing(Showing showing) {
+
+        if (showingRepository.existsByTheatreAndStartTime(showing.getTheatre(), showing.getStartTime())) {
+            throw new RuntimeException("Theatre already has a showing at this time!");
+        }
+        return showingRepository.save(showing);
+    }
+}
